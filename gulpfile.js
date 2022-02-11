a0c0e9683d19436d1b45cad40854218c681ea7ea
litvinenkoiryna <litvinenkoiryna@gmail.com>
2022-02-11 23:58:12 +0200

    First commit

---
+++ gulpfile.js
@@ -0,0 +1,37 @@
+let projectFolder = 'dist';
+let sourceFolder = 'src';
+
+let path = {
+    build: {
+        html: projectFolder + "/",
+        css: projectFolder + "/css/",
+        js: projectFolder + "/js/",
+    },
+    src: {
+        html: sourceFolder + "/",
+        css: sourceFolder + "/css/**/*.css",
+        js: sourceFolder + "/js/script.js/",
+    },
+    watch: {
+        html: sourceFolder + "/**/*.html",
+        css: sourceFolder + "/css/**/*.css",
+        js: sourceFolder + "/js/**/*.js/",
+    },
+    clean: "./" + projectFolder + "/"
+}
+let { src, dest } = require('gulp');
+
+const gulp = require('gulp');
+const autoprefixer = require('gulp-autoprefixer');
+
+gulp.task('prefixer', function (done) {
+    gulp.src('*.css')
+    return src(path.src.css)
+              .pipe(autoprefixer({
+                  browserlist: ['last 4 versions'],
+                  cascade: false
+        }))
+        .pipe(gulp.dest('dist'))
+        .pipe(dest(path.build.css));
+        done();
+});
